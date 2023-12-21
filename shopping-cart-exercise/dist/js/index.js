aded0ff09ab015aae8ac292fe139b8abad2c27f1
Pontus Segervall <pontus.segervall@hotmail.se>
2023-12-21 12:24:35 +0100

    First commit

--- shopping-cart-exercise/dist/js/index.js
+++ shopping-cart-exercise/dist/js/index.js
@@ -1,11 +1,51 @@
-const productButtons = document.querySelectorAll("button");
-const shoppingCart = [];
-const cart = document.querySelector("#cart");
-const openCartButton = document.querySelector("#open-cart");
-function updateCart() { }
-function addClickEvent() {
-    // Void betyder att funktionen ej returnerar något värde
-    // För att kunna använda parentElement behöver vi göra om det till ett HTMLElement istället för typen EventTarget
+var productButtons = document.querySelectorAll("button");
+var shoppingCart = [];
+var cart = document.querySelector("#cart");
+var openCartButton = document.querySelector("#open-cart");
+function updateCart() {
+    var productsInCart = document.getElementById("productsInCart");
+    productsInCart.textContent = shoppingCart.length.toString();
 }
-function listProductsInCart() { }
+function addClickEvent() {
+    Array.from(productButtons).forEach(function (button) {
+        button.addEventListener("click", function () {
+            var productName = button.getAttribute("data-product");
+            console.log(productName);
+            if (productName) {
+                // Check if the product is already in the shopping cart
+                var index = shoppingCart.indexOf(productName);
+                if (index !== -1) {
+                    // If the product is in the cart, you can handle it accordingly
+                    console.log("".concat(productName, " is already in the cart"));
+                }
+                else {
+                    // If the product is not in the cart, add it
+                    shoppingCart.push(productName);
+                    updateCart();
+                    listProductsInCart();
+                }
+            }
+        });
+    });
+}
+function listProductsInCart() {
+    var productsList = document.getElementById("products");
+    productsList.innerHTML = "";
+    // Add each product to the list
+    shoppingCart.forEach(function (product) {
+        var li = document.createElement("li");
+        li.textContent = product;
+        productsList.appendChild(li);
+    });
+}
+function openCart() {
+    var cartWrapper = document.getElementById("cart");
+    cartWrapper.classList.toggle("hide");
+}
+var openCartButtonHTMLElement = document.getElementById("open-cart");
+if (openCartButton) {
+    openCartButton.addEventListener("click", openCart);
+}
+updateCart();
+listProductsInCart();
 addClickEvent();
--- shopping-cart-exercise/src/index.ts
+++ shopping-cart-exercise/src/index.ts
@@ -3,13 +3,52 @@ const shoppingCart: (string | null)[] = [];
 const cart: HTMLElement | null = document.querySelector("#cart");
 const openCartButton: HTMLElement | null = document.querySelector("#open-cart");
 
-function updateCart(): void {}
-
-function addClickEvent(): void {
-  // Void betyder att funktionen ej returnerar något värde
-  // För att kunna använda parentElement behöver vi göra om det till ett HTMLElement istället för typen EventTarget
+function updateCart(): void {
+  const productsInCart = document.getElementById("productsInCart");
+  productsInCart.textContent = shoppingCart.length.toString();
 }
 
-function listProductsInCart(): void {}
+function addClickEvent(): void {
+  Array.from(productButtons as NodeListOf<HTMLElement>).forEach((button) => {
+    button.addEventListener("click", () => {
+      const productName: string = button.getAttribute("data-product");
+      console.log(productName);
+
+      if (productName) {
+        const index = shoppingCart.indexOf(productName);
+        if (index !== -1) {
+          console.log(`${productName} is already in the cart`);
+        } else {
+          shoppingCart.push(productName);
+          updateCart();
+          listProductsInCart();
+        }
+      }
+    });
+  });
+}
+
+function listProductsInCart(): void {
+  const productsList = document.getElementById("products");
+  productsList.innerHTML = "";
+  shoppingCart.forEach((product) => {
+    const li = document.createElement("li");
+    li.textContent = product;
+    productsList.appendChild(li);
+  })
+}
+
+function openCart() {
+  const cartWrapper = document.getElementById("cart");
+  cartWrapper.classList.toggle("hide");
+}
+const openCartButtonHTMLElement = document.getElementById("open-cart");
+if (openCartButton) {
+  openCartButton.addEventListener("click", openCart);
+}
+
+updateCart();
+listProductsInCart();
+
 
 addClickEvent();
